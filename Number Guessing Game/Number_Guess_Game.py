e0566fc83c9cbf8d35d314dc8712a251a18a71b3
CodeGoat29 <daviddipiazza29@gmail.com>
2025-11-16 21:43:06 -0500

    Added if and elif statments for anything below 1 and above 100

--- Number Guessing Game/Number_Guess_Game.py
+++ Number Guessing Game/Number_Guess_Game.py
@@ -1,4 +1,6 @@
 import random
+
+#Variables
 number = random.randint(1, 100)
 
 print ("This is the Number Guessing Game. You have 10 chances to guess the correct number between 1 and 100.")
@@ -6,7 +8,13 @@ attempts = 10
 while attempts >0:
     guess = int(input("Guess the Number: "))
 
-    if guess > number:
+    if guess > 100:
+        print ("Please select a number below 100")
+
+    elif guess < 1:
+        print ("Please select a number above 0")
+
+    elif guess > number:
         print ("lower")
 
     elif guess < number:
