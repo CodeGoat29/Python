bbc9763a3ce20c60300c50fbec07726d17445650
CodeGoat29 <daviddipiazza29@gmail.com>
2025-11-19 23:12:40 -0500

    Added my test file for learning python

---
+++ Test.py
@@ -0,0 +1,14 @@
+#Variables
+#Variables for strings need ""
+name = "David"
+#Variables for numbers do not need "" unless you want to act as an unchanged string
+age = 29
+birth = 2000
+
+#Print
+# A string is the words in bettween the ""
+print("Hello World!")
+#This will print 1 percent sign
+print("%")
+#Expression - This will print 10 percent signs
+print("%" * 10)
